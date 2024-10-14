2f33110b09b414bfdcffe36ba9aba789f49667a9
Michael Hanna <mh2parker@gmail.com>
2024-10-14 13:07:39 +0000

    chaning plotting scripts

--- plotting/behavioral.py
+++ plotting/behavioral.py
@@ -4,26 +4,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
-#%%
+
 model = 'pythia-70m-deduped'
 # maybe we want to change the display names of each model
-model_name_mapping = {'pythia-70m-deduped': 'pythia-70m-deduped', 'gemma-2-2b': 'gemma-2-2b'}
+model_name_mapping = {'pythia-70m-deduped': 'Pythia-70m', 'gemma-2-2b': 'Gemma-2-2B'}
 df = pd.read_csv(f'../results/{model}/gp_with_probs.csv')
 sns.set_theme(style='whitegrid')
 
-means = {f'{ambiguity}_{token}': [df[df['condition'] == category][f'{ambiguity}_{token}_prob'].mean() for category in ['NPZ', 'NPS', 'MVRR']] for ambiguity in ['ambiguous','gp','post'] for token in ['punct', 'was']}
+# means = {f'{ambiguity}_{token}': [df[df['condition'] == category][f'{ambiguity}_{token}_prob'].mean() for category in ['NPZ', 'NPS', 'MVRR']] for ambiguity in ['ambiguous','gp','post'] for token in ['punct', 'was']}
+means = {f'{ambiguity}_{token}': [df[df['condition'] == category][f'{ambiguity}_{token}_prob'].mean() for category in ['NPZ', 'NPS', 'MVRR']] for token in ['punct', 'was'] for ambiguity in ['ambiguous','gp','post']}
 
 categories = ['NP/Z', 'NP/S', 'MV/RR']
 x = np.arange(len(categories))  # the label locations
 width = 0.13  # the width of the bars
 multiplier = 0
 
-fig, ax =  plt.subplots()
-colors = ['lightcoral', 'firebrick', 'palegoldenrod', 'goldenrod', 'lightskyblue', 'dodgerblue']
-column_labels = [f'{ambiguity}, p({token})' for ambiguity in ['Ambiguous', 'GP', 'Non-GP'] for token in ['GP', 'non-GP']]
+fig, ax =  plt.subplots(figsize=(7,3))
+#colors = ['lightcoral', 'firebrick', 'palegoldenrod', 'goldenrod', 'lightskyblue', 'dodgerblue']
+colors = ['lightcoral', 'palegoldenrod', 'lightskyblue', 'firebrick', 'goldenrod', 'dodgerblue']
+column_labels = [f'{ambiguity}, p({token})'  for token in ['GP', 'non-GP'] for ambiguity in ['Ambiguous', 'GP', 'Non-GP']]
+handles = []
 for i, (attribute, measurement) in enumerate(means.items()):
     offset = width * multiplier
     rects = ax.bar(x + offset, measurement, width, label=column_labels[i], color=colors[i])
+    handles.append(rects)
     multiplier += 1
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
@@ -31,8 +35,8 @@ ax.set_ylabel('Probability')
 ax.set_xlabel('Garden Path Structure')
 ax.set_title(f'Garden Path Continuation Probabilities ({model_name_mapping[model]})')
 ax.set_xticks(x + width * (len(means) - 1)/2, categories)
-leg = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.12),
-          ncol=3)
+
+leg = ax.legend([handles[i] for i in [0,3,1,4,2,5]], [handles[i].get_label() for i in [0,3,1,4,2,5]], loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=3)
 Path(f'behavioral').mkdir(exist_ok=True, parents=True)
 plt.savefig(f'behavioral/{model}-behavioral.pdf', bbox_extra_artists=(leg,), bbox_inches='tight')
 plt.show()
--- plotting/causal.py
+++ plotting/causal.py
@@ -11,8 +11,8 @@ sns.set_style("whitegrid")
 model_name = 'pythia-70m-deduped'
 d = torch.load(f'../results/{model_name}/causal_probabilities.pt')
 
-model_name_mapping = {'pythia-70m-deduped': 'pythia-70m-deduped', 'gemma-2-2b': 'gemma-2-2b'}
-means = [[d[cond][struct][i].mean().item() for struct in ['NPZ', 'NPS']] for cond in ['intervened', 'baseline', 'random'] for i in [0, 1]]
+model_name_mapping = {'pythia-70m-deduped': 'Pythia-70m', 'gemma-2-2b': 'Gemma-2-2B'}
+means = [[d[cond][struct][i].mean().item() for struct in ['NPZ', 'NPS']]  for i in [0, 1] for cond in ['intervened', 'baseline', 'random']]
 
 categories = ['NP/Z', 'NP/S']
 x = np.arange(len(categories))  # the label locations
@@ -21,11 +21,15 @@ multiplier = 0
 
 
 fig, ax =  plt.subplots(figsize=(7,3))
-colors = ['lightcoral', 'firebrick', 'palegoldenrod', 'goldenrod', 'lightskyblue', 'dodgerblue']
-column_labels = [f'{ambiguity}, p({token})' for ambiguity in ['Intervention', 'Baseline', 'Random Int.'] for token in ['GP', 'non-GP']]
+#colors = ['lightcoral', 'firebrick', 'palegoldenrod', 'goldenrod', 'lightskyblue', 'dodgerblue']
+colors = ['lightcoral', 'palegoldenrod', 'lightskyblue', 'firebrick', 'goldenrod', 'dodgerblue']
+colors = ['olivedrab', 'mediumslateblue', 'orchid', 'darkolivegreen', 'darkslateblue', 'darkorchid']
+column_labels = [f'{ambiguity}, p({token})' for token in ['GP', 'non-GP'] for ambiguity in ['Intervention', 'Baseline', 'Random Int.']]
+handles = []
 for i, mean in enumerate(means):
     offset = width * multiplier
     rects = ax.bar(x + offset, mean, width, label=column_labels[i], color=colors[i])
+    handles.append(rects)
     multiplier += 1
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
@@ -33,7 +37,7 @@ ax.set_ylabel('Probability')
 ax.set_xlabel('Garden Path Structure')
 ax.set_title(f'Garden Path Continuation Probabilities ({model_name_mapping[model_name]})')
 ax.set_xticks(x + width * (len(means) - 1)/2, categories)
-leg = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.18), ncol=3)
+leg = ax.legend([handles[i] for i in [0,3,1,4,2,5]], [handles[i].get_label() for i in [0,3,1,4,2,5]], loc='upper center', bbox_to_anchor=(0.5, -0.18), ncol=3)
 
 Path(f'causal').mkdir(exist_ok=True, parents=True)
 fig.savefig(f'causal/{model_name}-causal.png', bbox_extra_artists=(leg,),bbox_inches='tight')
